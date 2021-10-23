bc169586a00fb1dccb3e2524326774b24a4618b0
Darius Morawiec <nok@users.noreply.github.com>
2021-10-23 19:15:43 +0200

    main: Add function to load multiple JavaScript libraries initially

--- pandas_render/__init__.py
+++ pandas_render/__init__.py
@@ -1,9 +1,13 @@
-from typing import Optional
+from typing import Optional, Union, List, Tuple
+from collections import namedtuple
+from inspect import cleandoc
 
+from IPython.display import Javascript  # noqa
+from jinja2 import Template as JinjaTemplate
 import pandas  # noqa
 
 
-def init(backend: Optional[str] = None):
+def _handle_extensions():
     if not hasattr(pandas.Series, 'render'):
         from pandas_render.extensions.Series import render as _render_series
         setattr(pandas.Series, 'render', _render_series)
@@ -12,23 +16,63 @@ def init(backend: Optional[str] = None):
         from pandas_render.extensions.DataFrame import render as _render_dataframe
         setattr(pandas.DataFrame, 'render', _render_dataframe)
 
-    if backend and backend == 'alpine':
-        from IPython.display import Javascript
-        return Javascript("""
-            var loadScriptSync = function(src, scope) {{
-                if (!scope) {{
-                    var script = document.createElement('script');
-                    script.src = src;
-                    script.type = 'text/javascript';
-                    script.async = false;
-                    document.getElementsByTagName('head')[0].appendChild(script);
-                }}
-            }};
-            loadScriptSync("{src}", {scope});
-        """.format(
-            **dict(src='https://unpkg.com/alpinejs@3.4.2/dist/cdn.min.js', scope='window.Alpine')))
+
+def _handle_libraries(
+        libraries: Union[str, Tuple[str, str], List[Union[str, Tuple[str, str]]]]) -> str:
+    Library = namedtuple('Library', 'src scope')
+
+    if isinstance(libraries, (str, tuple)):
+        libraries = [libraries]
+
+    valid_libraries: List[Library] = []
+    if isinstance(libraries, list):
+        for library in libraries:
+            if isinstance(library, str):
+                if library == 'alpine':
+                    library = Library(
+                        src='https://unpkg.com/alpinejs@3.4.2/dist/cdn.min.js',
+                        scope='window.Alpine')
+                else:
+                    library = Library(src=library, scope='null')
+            elif isinstance(library, tuple) and len(library) >= 2:
+                library = Library(src=library[0], scope=library[1])
+
+            if isinstance(library, Library):
+                valid_libraries.append(library)
+
+    template = JinjaTemplate(
+        cleandoc("""
+        var loadScriptSync = function(src, scope) {
+            if (scope != null && !scope) {
+                var script = document.createElement('script');
+                script.src = src;
+                script.type = 'text/javascript';
+                script.async = false;
+                document.getElementsByTagName('head')[0].appendChild(script);
+            }
+        };
+        {% for library in libraries %}
+        loadScriptSync("{{ library.src }}", {{ library.scope }});
+        {% endfor %}
+    """))
+
+    output = ''
+    if len(valid_libraries):
+        output = template.render(libraries=valid_libraries)
+    return output
+
+
+def init(libraries: Optional[Union[str, Tuple[str, str], List[Union[str, Tuple[str, str]]]]] = None,
+         return_str: bool = False) -> Optional[Union[str, Javascript]]:
+    _handle_extensions()
+    if libraries:
+        output = _handle_libraries(libraries)
+        if return_str:
+            return output
+        return Javascript(output)
+    return None
 
 
 __version__ = '0.1.0'
 
-init()  # default initialization of the extension
+init()  # default initialization without any additional libraries
--- tests/test_pandas_render.py
+++ tests/test_pandas_render.py
@@ -9,3 +9,31 @@ def test_implicit_initialization():
 
     assert hasattr(pandas.Series, 'render')
     assert hasattr(pandas.DataFrame, 'render')
+
+
+def test_initialization_with_library():
+    from IPython.display import Javascript  # noqa
+    from pandas_render import init  # noqa
+
+    output = init('alpine')
+    assert type(output) == Javascript
+
+    alpine = 'https://unpkg.com/alpinejs@3.4.2/dist/cdn.min.js'
+
+    output = init('alpine', return_str=True)
+    assert type(output) == str
+    assert f'loadScriptSync("{alpine}", window.Alpine);' in output
+
+    output = init((alpine, 'window.Alpine'), return_str=True)
+    assert f'loadScriptSync("{alpine}", window.Alpine);' in output
+
+    output = init([(alpine, 'window.Alpine')], return_str=True)
+    assert f'loadScriptSync("{alpine}", window.Alpine);' in output
+
+    output = init(alpine, return_str=True)
+    assert f'loadScriptSync("{alpine}", null);' in output
+
+    output = init(['alpine', ('a.js', 'window.a'), 'b.js'], return_str=True)
+    assert f'loadScriptSync("{alpine}", window.Alpine);' in output
+    assert f'loadScriptSync("a.js", window.a);' in output
+    assert f'loadScriptSync("b.js", null);' in output
